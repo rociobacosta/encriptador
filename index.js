f58b3e6b313563fba406864346976b9bb606633d
Rocio Acosta <121262117+rooacost@users.noreply.github.com>
2024-03-15 22:39:35 -0300

    primer creacion de index.html e index.css

---
+++ index.js
@@ -0,0 +1,16 @@
+let textarea = document.getElementsByClassName(textarea);
+
+const e = "enter";
+const i = "imes";
+const a = "ai";
+const o = "ober";
+const u = "ufat";
+
+switch (key) {
+  case "a":
+    a = "ai"
+    break;
+
+  default:
+    break;
+}
